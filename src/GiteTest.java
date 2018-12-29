029fc0b98e0ed103cd26b8e99ebc88d9fde11dcb
zhao <814124246@qq.com>
2018-12-29 14:16:27 +0800

    first commit

---
+++ src/GiteTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: 赵佟
+ * @Date: 2018/12/29 14:08
+ * @Description:
+ */
+public class GiteTest {
+    public static void main(String[] args) {
+        System.out.println("hello  " );
+    }
+}
